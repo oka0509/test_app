da113e0ec93e1a397a732b8217b59c5f17d555b2
oka1791 <you@example.com>
2023-12-04 01:57:40 +0900

    (#2) feat: 日本語対応のseedファイルを追加

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ module TestApp
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+    Rails.application.config.i18n.default_locale = :ja
+    Faker::Config.locale = :ja
 
     # Configuration for the application, engines, and railties goes here.
     #
