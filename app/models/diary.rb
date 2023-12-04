b4a4440ed77e1101c885fd4f6aa13b9bfd87c759
oka1791 <you@example.com>
2023-12-04 21:05:19 +0900

    (#14) feat: テキストでの検索機能を追加

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -10,4 +10,8 @@ class Diary < ApplicationRecord
   def self.ransackable_attributes(_auth_object = nil)
     ['content']
   end
+
+  def self.ransackable_associations(_auth_object = nil)
+    %w[user tags]
+  end
 end
