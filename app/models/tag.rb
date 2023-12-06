dff2ac39d3890a24b9990c0c6ce85fe52056beae
oka1791 <76876781+oka0509@users.noreply.github.com>
2023-12-07 00:45:38 +0900

    (#10) feat: showの機能を追加

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,6 @@
 class Tag < ApplicationRecord
   has_many :tag_diaries, dependent: :destroy
+  has_many :diaries, through: :tag_diaries
 
   def self.ransackable_attributes(_auth_object = nil)
     ['name']
