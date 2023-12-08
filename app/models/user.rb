c0600895824f9ecd1b35241157f9de7082df55a2
oka1791 <76876781+oka0509@users.noreply.github.com>
2023-12-08 22:50:48 +0900

    (#22) feat: バリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   has_secure_password
   has_many :diaries, dependent: :destroy
 
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+
   def self.ransackable_attributes(_auth_object = nil)
     ['name']
   end
