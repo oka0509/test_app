5695239eff2f420d26daab5cc84714a70de9d4c6
oka1791 <76876781+oka0509@users.noreply.github.com>
2023-12-08 18:10:33 +0900

    (#20) fix: tooltipのバグを修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,7 +4,6 @@
 
 import "@hotwired/turbo-rails"
 
-
 $(document).on('turbo:load', function() {
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
     tooltipTriggerList.forEach(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
