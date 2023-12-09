774ec5fa51cee49f869864802ce3e15e5b5d56f4
oka1791 <76876781+oka0509@users.noreply.github.com>
2023-12-09 17:05:09 +0900

    (#22) feat: 日記が空白の時にクライアント側で無効化

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,6 +5,19 @@
 import "@hotwired/turbo-rails"
 
 $(document).on('turbo:load', function() {
-    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-    tooltipTriggerList.forEach(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    tooltipTriggerList.forEach(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+    const diaryAddButton = $('#diary-create-button');
+    diaryAddButton.prop("disabled", true);
+    console.log(diaryAddButton)
+    $('#diary-content').keyup(function() {
+        if($(this)[0].value == '') {
+            console.log('if')
+            diaryAddButton.prop("disabled", true);
+        } else {
+            console.log('else')
+            diaryAddButton.prop("disabled", false);
+        }
+    });
 })
