65b3a5232cb8940ddaf061afdf26da18a45e34c6
oka1791 <76876781+oka0509@users.noreply.github.com>
2023-12-25 20:58:42 +0900

    これでいけるかな...

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module TestApp
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+    config.i18n.available_locales = [:ja]
     config.i18n.default_locale = :ja
 
     # Configuration for the application, engines, and railties goes here.
