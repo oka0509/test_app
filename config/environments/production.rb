3dbce988e8e42642f14c217592939e5ec75e0c26
oka1791 <76876781+oka0509@users.noreply.github.com>
2023-12-26 20:48:04 +0900

    日数でローテートする

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,7 @@ Rails.application.configure do
   # information to avoid inadvertent exposure of personally identifiable information (PII). If you
   # want to log everything, set the level to "debug".
   config.log_level = ENV.fetch('RAILS_LOG_LEVEL', 'info')
+  config.logger = Logger.new('log/production.log', 'daily')
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
